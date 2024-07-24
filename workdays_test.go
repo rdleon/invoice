4bb3cdf63440b111afb3767c3eacce274d123bc0
Rafael Diaz de Leon <r@rdleon.me>
2024-07-24 08:07:34 -0700

    Add more weekdays calculation tests

--- workdays_test.go
+++ workdays_test.go
@@ -13,9 +13,14 @@ func TestWorkDays(t *testing.T) {
 
 	tests := []test{
 		{time.Date(2023, 2, 1, 0, 0, 0, 0, time.UTC), 20},
+		{time.Date(2024, 2, 4, 0, 0, 0, 0, time.UTC), 21},
+		{time.Date(2023, 2, 28, 0, 0, 0, 0, time.UTC), 20},
 		{time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC), 21},
+		{time.Date(2024, 2, 28, 0, 0, 0, 0, time.UTC), 21},
+		{time.Date(2024, 2, 29, 0, 0, 0, 0, time.UTC), 21},
 		{time.Date(2024, 7, 1, 0, 0, 0, 0, time.UTC), 23},
 		{time.Date(2024, 7, 8, 0, 0, 0, 0, time.UTC), 23},
+		{time.Date(2024, 7, 31, 0, 0, 0, 0, time.UTC), 23},
 		{time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC), 20},
 		{time.Date(2024, 5, 1, 0, 0, 0, 0, time.UTC), 23},
 	}
