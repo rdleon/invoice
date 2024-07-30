37ce4e5a230582370dbf12ef47bc74d398c206b1
Rafael Diaz de Leon <r@rdleon.me>
2024-07-29 19:35:02 -0700

    Basic command line functionality

--- invoice.go
+++ invoice.go
@@ -2,20 +2,72 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/shopspring/decimal"
 )
 
+type Item struct {
+	quantity    decimal.Decimal
+	cost        decimal.Decimal
+	description string
+	subtotal    decimal.Decimal
+}
+
+func outputText(item Item) {
+	fmt.Println("Description, quantity, rate, total")
+	fmt.Printf("%s, %s, %s, %s\n", item.description, item.quantity.StringFixed(0), item.cost.StringFixedBank(2), item.subtotal.StringFixedBank(2))
+}
+
+func outputHtml(item Item) {
+	fmt.Printf("<td>%s</td><td>%s</td><td>%s</td><td>%s</td>\n", item.description, item.quantity.StringFixed(0), item.cost.StringFixedBank(2), item.subtotal.StringFixedBank(2))
+}
+
 func main() {
+	var daysOutOfOffice int
+	var htmlOutput bool
+
+	flag.IntVar(&daysOutOfOffice, "out-of-office", 0, "Days not worked this month")
+	flag.BoolVar(&htmlOutput, "html", false, "Output the invoice in HTML")
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 2 {
+		fmt.Fprintln(os.Stderr, "Wrong number of arguments")
+		return
+	}
+
 	// Use Decimal for money
-	var hourlyRate decimal.Decimal
+	var item Item
+	var err error
 
-	hourlyRate, _ = decimal.NewFromString("10.07")
+	var workDays = weekdays(time.Now()) - daysOutOfOffice
 
-	var workDays = weekdays(time.Now())
-	var cost = decimal.NewFromInt(int64(workDays)).Mul(hourlyRate)
+	if workDays < 0 {
+		workDays = 0
+	}
 
-	fmt.Printf("workdays %d\ncost %s\n", workDays, cost.StringFixedBank(2))
+	item.description = args[0]
+
+	item.cost, err = decimal.NewFromString(args[1])
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error parsing the daily rate")
+		return
+	}
+
+	item.quantity = decimal.NewFromInt(int64(workDays))
+	item.subtotal = item.quantity.Mul(item.cost)
+
+	if htmlOutput {
+		outputHtml(item)
+		return
+	}
+
+	outputText(item)
 }
